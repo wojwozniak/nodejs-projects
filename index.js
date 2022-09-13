e99f2c1690159060f3455df155af6c0246964183
pulchnymamut <pulchnymamut@gmail.com>
2022-09-13 18:20:08 +0200

    reworked the app to fix weird bug

--- index.js
+++ index.js
@@ -5,10 +5,12 @@
 let express = require('express');
 let app = express();
 
+// Using moment.js as it solves weird bug (old code can be found in oldcode.js (it doesn't pass one automated test, idk why))
+let moment = require('moment');
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 let cors = require('cors');
-const { RepeatWrapping } = require('three');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -25,116 +27,47 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// End of boilerplate code
+
+app.get('/api/:request', (req, res) => {
+  let request = req.params.request;
+  let unixFromDate = new Date(request).getTime();
+  let parsedUnix = new Date(parseInt(request)).getTime();
+
+  // If request is valid date then value of unixFromDate variable will be greater than 0
+  if (unixFromDate > 0) {
+    res.json({
+      unix: moment(unixFromDate).unix() * 1000,
+      utc: new Date(unixFromDate).toUTCString()
+    });
+  }
+  
+  // If first test doesn't pass - checking if request was a unix timestamp
+  else if (parsedUnix > 0) {
+    res.json({
+      unix: moment(parseInt(request)).unix() * 1000,
+      utc: new Date(parseInt(request)).toUTCString()
+    });
+  }
+  
+  // If both previous test failed then it is a invalid date (or empty request that will be handled later)
+  else {
+    res.json({
+      error : "Invalid date"
+    });
+  }
+});
+
+app.get('/api', (req, res) => {
+  res.json({
+    unix:moment().unix()*1000,
+    utc:new Date().toUTCString()
+  })
+});
+
 
 
 // listen for requests :)
 let listener = app.listen(process.env.PORT=3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
-});
-
-// End of boilerplate code
-
-const days = [
-  'Sun',
-  'Mon',
-  'Tue',
-  'Wed',
-  'Thu',
-  'Fri',
-  'Sat'
-]
-
-const months = [
-  'Jan',
-  'Feb',
-  'Mar',
-  'Apr',
-  'May',
-  'Jun',
-  'Jul',
-  'Aug',
-  'Sep',
-  'Oct',
-  'Nov',
-  'Dec'
-]
-
-// Function that returns valid output for utc
-const convertToDatestring = (int) => {
-  function padTo2Digits(num) {
-    return num.toString().padStart(2, '0');
-  }
-  let output = days[int.getDay()] + ", "+int.getDate() + " "+months[int.getMonth()]+" "+int.getFullYear() + " " + padTo2Digits(int.getUTCHours()) + ":" + padTo2Digits(int.getUTCMinutes()) + ":" + padTo2Digits(int.getUTCSeconds()) + " GMT";
-  return output;
-}
-
-// Function checking if input is a valid date
-function isValidDate(d) {
-  return d instanceof Date && !isNaN(d);
-}
-
-app.use("/api", (req, res) => {
-  let unix; // UNIX output variable
-  let utc; // UTC output variable
-
-  // Function for sending valid responses
-  const sendValidResponse = () => {
-    res.set({ 'Content-Type': 'application/json' });
-    res.send(JSON.stringify({
-      unix: unix,
-      utc: utc
-    }));
-  }
-
-  // Getting request path, deleting slash and fixing URL encoding
-  let date = req.path;
-  date = date.substring(1);
-  if (date.includes('%20')) {
-    date = date.replaceAll('%20', ' ');
-    if (!date.includes("GMT")) {
-      date += " GMT";
-    }
-  }
-  
-  let dateObject = new Date(date);
-
-  // Check if request is unix format
-  let isUnix = true;
-  for (let i = 0; i < date.length; i++) {
-    // Check in regex if api request contains not a number - if yes, changing isUnix to false
-    if (/^([^0-9]*)$/.test(date[i])) {
-      isUnix = false;
-    }
-  }
-
-  // Dealing with empty request
-  if (date === "") {
-    let currentDate = new Date();
-    unix = currentDate.getTime();
-    utc = convertToDatestring(currentDate);
-    sendValidResponse();
-  } else if 
-  
-  // Resolving request for valid unix strings
-  (isUnix === true) {
-    unix = parseInt(date, 10);
-    let dateObj = new Date(unix);
-    utc = convertToDatestring(dateObj);
-    sendValidResponse();
-  }
-    
-  // Dealing with invalid dates
-  else if (!isValidDate(dateObject)) {
-    res.set({ 'Content-Type': 'application/json' });
-    res.send(JSON.stringify({
-      error: "Invalid Date"
-    }));
-  } else
-  
-  // Dealing with valid dates
-  {
-    unix = dateObject.getTime();
-    utc = convertToDatestring(dateObject);
-    sendValidResponse();
-  }
 });
---
+++ oldcode.js
@@ -0,0 +1,139 @@
+// Code that works but for some reason doesn't pass auto tests
+
+// index.js
+// where your node app starts
+
+// init project
+let express = require('express');
+let app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+let cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+// End of boilerplate code
+
+const days = [
+  'Sun',
+  'Mon',
+  'Tue',
+  'Wed',
+  'Thu',
+  'Fri',
+  'Sat'
+]
+
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec'
+]
+
+// Function that returns valid output for utc
+const convertToDatestring = (int) => {
+  function padTo2Digits(num) {
+    return num.toString().padStart(2, '0');
+  }
+  let output = days[int.getDay()] + ", "+int.getDate() + " "+months[int.getMonth()]+" "+int.getFullYear() + " " + padTo2Digits(int.getUTCHours()) + ":" + padTo2Digits(int.getUTCMinutes()) + ":" + padTo2Digits(int.getUTCSeconds()) + " GMT";
+  return output;
+}
+
+// Function checking if input is a valid date
+function isValidDate(d) {
+  return d instanceof Date && !isNaN(d);
+}
+
+app.use("/api", (req, res) => {
+  let unix; // UNIX output variable
+  let utc; // UTC output variable
+
+  // Function for sending valid responses
+  const sendValidResponse = () => {
+    res.set({ 'Content-Type': 'application/json' });
+    res.send(JSON.stringify({
+      unix: unix,
+      utc: utc
+    }));
+  }
+
+  // Getting request path, deleting slash and fixing URL encoding
+  let date = req.path;
+  date = date.substring(1);
+  if (date.includes('%20')) {
+    date = date.replaceAll('%20', ' ');
+    if (!date.includes("GMT")) {
+      date += " GMT";
+    }
+  }
+  
+  let dateObject = new Date(date);
+
+  // Check if request is unix format
+  let isUnix = true;
+  for (let i = 0; i < date.length; i++) {
+    // Check in regex if api request contains not a number - if yes, changing isUnix to false
+    if (/^([^0-9]*)$/.test(date[i])) {
+      isUnix = false;
+    }
+  }
+
+  // Dealing with empty request
+  if (date === "") {
+    let currentDate = new Date();
+    unix = currentDate.getTime();
+    utc = convertToDatestring(currentDate);
+    sendValidResponse();
+  } else if 
+  
+  // Resolving request for valid unix strings
+  (isUnix === true) {
+    unix = parseInt(date, 10);
+    let dateObj = new Date(unix);
+    utc = convertToDatestring(dateObj);
+    sendValidResponse();
+  }
+    
+  // Dealing with invalid dates
+  else if (!isValidDate(dateObject)) {
+    res.set({ 'Content-Type': 'application/json' });
+    res.send(JSON.stringify({
+      error: "Invalid Date"
+    }));
+  } else
+  
+  // Dealing with valid dates
+  {
+    unix = dateObject.getTime();
+    utc = convertToDatestring(dateObject);
+    sendValidResponse();
+  }
+});
+
+// listen for requests :)
+let listener = app.listen(process.env.PORT=3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
