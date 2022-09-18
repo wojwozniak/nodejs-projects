a68a61ae9844e22207e69c98dd5d8c22a55f2ef2
pulchnymamut <pulchnymamut@gmail.com>
2022-09-18 14:24:18 +0200

    All tests passing

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+
+const multer = require('multer');
+const uploaded = multer({ dest: 'uploads/' });
+
 
 var app = express();
 
@@ -11,10 +15,22 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
-
 const port = process.env.PORT || 3000;
+
+app.post('/api/fileanalyse', uploaded.single('upfile'), (req, res) => {
+  try {
+    res.json({
+      "name": req.file.originalname,
+      "type": req.file.mimetype,
+      "size": req.file.size
+    });
+  } catch (err) {
+    res.send(400);
+  }
+
+  res.send("placeholder");
+});
+
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
