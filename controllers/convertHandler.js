3abf5bb1a3776f596ff2eaa9ef6cfffcb36b338b
Wojtek <pulchnymamut@gmail.com>
2023-05-22 16:34:06 +0200

    fix comments, add convert function

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,6 +1,7 @@
-// First part of the project - conversion logic
-
-
+/**
+ * This function handles the conversion of 
+ * units - it's main logic part of the app
+ */
 function ConvertHandler() {
   
   this.getNum = (input) => {
@@ -21,7 +22,12 @@ function ConvertHandler() {
     return result;
   };
 
-  // Function spelling out the units (used in the getString function)
+  /**
+   * This function returns the spelled out unit
+   * @param {*} initUnit - the unit to convert
+   * @param {*} returnUnit - the unit to convert to
+   * @returns the spelled out unit to use in toString method
+   */
   this.spellOutUnit = (initUnit, returnUnit) => {
     const units = {
       gal: 'gallons',
@@ -34,13 +40,35 @@ function ConvertHandler() {
     return [units[initUnit], units[returnUnit]];
   };
   
+  /**
+   * This function converts the initNum to 
+   * the returnNum
+   * @param {*} initNum - the number to convert
+   * @param {*} initUnit - the unit to convert
+   * @returns the converted number
+   */
   this.convert = (initNum, initUnit) => {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    let result;
-    
-    return result;
+    const conversions = {
+      'gal': 3.78541,
+      'lbs': 0.453592,
+      'mi': 1.60934,
+      'l': 1 / 3.78541,
+      'kg': 1 / 0.453592,
+      'km': 1 / 1.60934
+    };
+  
+    if (initNum === 'invalid number' || initUnit === 'invalid unit' || initUnit === 'no unit') {
+      return;
+    }
+  
+    const conversionFactor = conversions[initUnit];
+  
+    if (conversionFactor) {
+      const result = initNum * conversionFactor;
+      return result.toFixed(5);
+    }
+  
+    return initNum;
   };
   
   this.getString = (initNum, initUnit, returnNum, returnUnit) => {
